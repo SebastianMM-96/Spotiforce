c9c36caeb0e0ba638e27c4e6987690e332ced749
Sebastian Marroquin <sebasmarro10@gmail.com>
2021-10-12 11:10:10 -0500

    Custom components for artist

---
+++ Spotiforce/force-app/main/default/aura/ArtistCreateAlbum/ArtistCreateAlbumController.js
@@ -0,0 +1,16 @@
+({
+    handleError: function (cmp, event, helper) {
+        cmp.find('notifLib').showToast({
+            "title": "Something has gone wrong!",
+            "message": event.getParam("message"),
+            "variant": "error"
+        });
+    },
+    handleSuccess : function(component, event, helper) {
+        component.find('notifLib').showToast({
+            "variant": "success",
+            "title": "Account Created",
+            "message": "Record ID: " + event.getParam("id")
+        });
+    }
+})
